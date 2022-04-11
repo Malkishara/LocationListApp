0703d1a4c798911ffdf408a404dc6bfde6654b63
Malkishara <isurumalkishara97@gmail.com>
2022-04-11 09:23:02 +0530

    'Added_project'

---
+++ app/src/main/java/com/example/locationlistapp/LocationModel.kt
@@ -0,0 +1,18 @@
+package com.example.locationlistapp
+
+import java.util.*
+
+data class LocationModel (var id: Int = getAutoId(),
+                          var loc:String = "",
+                          var longitude:String = "",
+                          var latitude:String = ""){
+
+    companion object
+    {
+        fun getAutoId():Int{
+            val random = Random()
+            return random.nextInt(100)
+        }
+
+    }
+}
